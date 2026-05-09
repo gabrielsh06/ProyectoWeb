3fef985211883a150d9f1f633c9b0087ad0bf701
gabrielsh06 <shgabriel06@gmail.com>
2026-05-09 11:31:06 -0500

    agregar controller y service para product

---
+++ src/main/java/com/proyecto/web/product/controller/ProductController.java
@@ -0,0 +1,60 @@
+package com.proyecto.web.product.controller;
+
+import com.proyecto.web.domain.Product;
+import com.proyecto.web.product.dto.request.ProductCreateRequest;
+import com.proyecto.web.product.dto.request.ProductUpdateRequest;
+import com.proyecto.web.product.dto.response.ProductResponse;
+import com.proyecto.web.product.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable Long id) {
+
+        ProductResponse response = productService.getProduct(id);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+
+        List<ProductResponse> response = productService.getAllProducts();
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> registerProduct(@Valid @RequestBody ProductCreateRequest productCreateRequest) {
+
+        ProductResponse response = productService.registerProduct(productCreateRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id,
+                                                         @Valid @RequestBody ProductUpdateRequest productUpdateRequest) {
+
+        ProductResponse response = productService.updateProduct(id,  productUpdateRequest);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/proyecto/web/product/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.proyecto.web.product.service;
+
+import com.proyecto.web.product.dto.request.ProductCreateRequest;
+import com.proyecto.web.product.dto.request.ProductUpdateRequest;
+import com.proyecto.web.product.dto.response.ProductResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    public ProductResponse getProduct(Long id) {
+
+        return null;
+    }
+
+    public List<ProductResponse> getAllProducts() {
+
+        return null;
+    }
+
+    public ProductResponse registerProduct(ProductCreateRequest productCreateRequest) {
+
+        return null;
+    }
+
+    public ProductResponse updateProduct(Long id, ProductUpdateRequest productUpdateRequest) {
+
+        return null;
+    }
+
+    public void deleteProduct(Long id) {
+
+    }
+}
