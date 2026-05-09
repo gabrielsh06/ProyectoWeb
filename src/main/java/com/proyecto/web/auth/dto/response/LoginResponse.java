27dae686042fa6977123480b1979820179a92807
gabrielsh06 <shgabriel06@gmail.com>
2026-05-09 11:37:07 -0500

    refactor: modificar el LoginResponse para devolver el rol

--- src/main/java/com/proyecto/web/auth/dto/response/LoginResponse.java
+++ src/main/java/com/proyecto/web/auth/dto/response/LoginResponse.java
@@ -1,14 +1,10 @@
 package com.proyecto.web.auth.dto.response;
 
-import com.proyecto.web.domain.User;
 
 public record LoginResponse(
 
         Long id,
-        String username
+        String username,
+        String role
 ) {
-    public LoginResponse(User user) {
-        this(user.getId(),
-                user.getUsername());
-    }
 }
