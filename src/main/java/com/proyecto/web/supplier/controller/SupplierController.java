8872ed09c03fcf5b8f0a6a2560caff062a795a70
gabrielsh06 <shgabriel06@gmail.com>
2026-05-08 20:03:51 -0500

    agregar controller y service de supplier

---
+++ src/main/java/com/proyecto/web/supplier/controller/SupplierController.java
@@ -0,0 +1,43 @@
+package com.proyecto.web.supplier.controller;
+
+import com.proyecto.web.supplier.dto.request.SupplierCreateRequest;
+import com.proyecto.web.supplier.dto.request.SupplierUpdateRequest;
+import com.proyecto.web.supplier.dto.response.SupplierResponse;
+import com.proyecto.web.supplier.service.SupplierService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/supplier")
+public class SupplierController {
+
+    private final SupplierService supplierService;
+
+    public SupplierController(SupplierService supplierService) {
+        this.supplierService = supplierService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<SupplierResponse> registerSupplier(
+            @Valid @RequestBody SupplierCreateRequest supplierCreateRequest) {
+
+        SupplierResponse response = supplierService.registerSupplier(supplierCreateRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<SupplierResponse> updateSupplier(@PathVariable Long id,
+                                                                @Valid @RequestBody SupplierUpdateRequest supplierUpdateRequest) {
+
+        SupplierResponse response = supplierService.updateSupplier(id, supplierUpdateRequest);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteSupplier(@PathVariable Long id) {
+
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ src/main/java/com/proyecto/web/supplier/service/SupplierService.java
@@ -0,0 +1,24 @@
+package com.proyecto.web.supplier.service;
+
+import com.proyecto.web.supplier.dto.request.SupplierCreateRequest;
+import com.proyecto.web.supplier.dto.request.SupplierUpdateRequest;
+import com.proyecto.web.supplier.dto.response.SupplierResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SupplierService {
+
+    public SupplierResponse registerSupplier(SupplierCreateRequest supplierCreateRequest) {
+
+        return null;
+    }
+
+    public SupplierResponse updateSupplier(Long id, SupplierUpdateRequest supplierUpdateRequest) {
+
+        return null;
+    }
+
+    public void deleteSupplier(Long id) {
+
+    }
+}
