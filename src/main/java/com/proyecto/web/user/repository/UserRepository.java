9d164ecd46f55b90712f41ef4a1fb90d2d1cb619
gabrielsh06 <shgabriel06@gmail.com>
2026-05-09 16:53:53 -0500

    agregar repository para user

---
+++ src/main/java/com/proyecto/web/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.proyecto.web.user.repository;
+
+import com.proyecto.web.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAllByStateTrue();
+}
