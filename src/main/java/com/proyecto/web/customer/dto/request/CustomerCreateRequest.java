4a41b53bdc8769d5087af330a03a5b357a97d764
gabrielsh06 <shgabriel06@gmail.com>
2026-05-09 18:19:29 -0500

    Agregar método para hacer el cambio de DTO a customer

--- src/main/java/com/proyecto/web/customer/dto/request/CustomerCreateRequest.java
+++ src/main/java/com/proyecto/web/customer/dto/request/CustomerCreateRequest.java
@@ -1,5 +1,6 @@
 package com.proyecto.web.customer.dto.request;
 
+import com.proyecto.web.domain.Customer;
 import jakarta.validation.constraints.NotBlank;
 
 public record CustomerCreateRequest(
@@ -9,4 +10,10 @@ public record CustomerCreateRequest(
         @NotBlank(message = "EL apellido no puede estar en blanco")
         String lastName
 ) {
+        public Customer toEntity() {
+            Customer customer = new Customer();
+            customer.setFirstName(firstName);
+            customer.setLastName(lastName);
+            return customer;
+        }
 }
