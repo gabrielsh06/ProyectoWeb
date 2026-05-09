886251950ac8cd5c7dedb8e7b112541db714847d
gabrielsh06 <shgabriel06@gmail.com>
2026-05-09 18:18:48 -0500

    agregar repository para customer

---
+++ src/main/java/com/proyecto/web/customer/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.proyecto.web.customer.repository;
+
+import com.proyecto.web.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findAllByStateTrue();
+}
