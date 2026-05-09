0613915fc2b34b559f1e554f2f025472ceef5301
gabrielsh06 <shgabriel06@gmail.com>
2026-05-08 21:14:40 -0500

    Agregar validación a los DTO de user

--- src/main/java/com/proyecto/web/user/dto/request/UserCreateRequest.java
+++ src/main/java/com/proyecto/web/user/dto/request/UserCreateRequest.java
@@ -1,11 +1,14 @@
 package com.proyecto.web.user.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserCreateRequest(
 
         @NotBlank(message = "El nombre de usuario no puede estar en blanco")
         String username,
+        @NotBlank(message = "El correo no puede estar en blanco")
+        @Email(message = "El correo debe estar bien escrito")
         String email,
         @NotBlank(message = "La contraseña no puede estar en blanco")
         String password,
--- src/main/java/com/proyecto/web/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/proyecto/web/user/dto/request/UserUpdateRequest.java
@@ -1,11 +1,20 @@
 package com.proyecto.web.user.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UserUpdateRequest(
 
+        @NotBlank(message = "El nombre de usuario no puede estar en blanco")
         String username,
+        @NotBlank(message = "El correo no puede estar en blanco")
+        @Email(message = "El correo debe estar bien escrito")
         String email,
+        @NotBlank(message = "La contraseña no peude estar en blanco")
         String password,
+        @NotBlank(message = "El nombre no puede estar en blanco")
         String firstName,
+        @NotBlank(message = "El rol no puede estar en blanco")
         String role
 ) {
 }
