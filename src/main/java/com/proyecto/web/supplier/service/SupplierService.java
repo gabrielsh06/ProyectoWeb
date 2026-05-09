2de89009e1357aa2b38bbce76e5e285e3353a28e
gabrielsh06 <shgabriel06@gmail.com>
2026-05-09 18:51:55 -0500

    agregar lógica para el service de supplier

--- src/main/java/com/proyecto/web/supplier/service/SupplierService.java
+++ src/main/java/com/proyecto/web/supplier/service/SupplierService.java
@@ -1,8 +1,11 @@
 package com.proyecto.web.supplier.service;
 
+import com.proyecto.web.domain.Supplier;
 import com.proyecto.web.supplier.dto.request.SupplierCreateRequest;
 import com.proyecto.web.supplier.dto.request.SupplierUpdateRequest;
 import com.proyecto.web.supplier.dto.response.SupplierResponse;
+import com.proyecto.web.supplier.repository.SupplierRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,27 +13,55 @@ import java.util.List;
 @Service
 public class SupplierService {
 
+    private final SupplierRepository supplierRepository;
+
+    public SupplierService(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
     public SupplierResponse getSupplier(Long id) {
 
-        return null;
+        return supplierRepository.findById(id)
+                .filter(Supplier::getState)
+                .map(SupplierResponse::new)
+                .orElseThrow(() -> new EntityNotFoundException("Supplier not found"));
     }
 
     public List<SupplierResponse> getAllSuppliers() {
 
-        return null;
+        return supplierRepository.findAllByStateTrue().stream()
+                .map(SupplierResponse::new)
+                .toList();
     }
 
     public SupplierResponse registerSupplier(SupplierCreateRequest supplierCreateRequest) {
 
-        return null;
+        Supplier supplier = supplierCreateRequest.toEntity();
+        supplier.setState(true);
+
+        return new SupplierResponse(supplierRepository.save(supplier));
     }
 
     public SupplierResponse updateSupplier(Long id, SupplierUpdateRequest supplierUpdateRequest) {
 
-        return null;
+        Supplier supplier = supplierRepository.findById(id)
+                .filter(Supplier::getState)
+                .orElseThrow(() -> new EntityNotFoundException("Supplier not found"));
+        supplier.setSupplierName(supplierUpdateRequest.supplierName());
+        supplier.setDateLastOrder(supplierUpdateRequest.dateLastOrder());
+        supplier.setDebt(supplierUpdateRequest.debt());
+        supplier.setPaymentStatus(supplierUpdateRequest.paymentStatus());
+
+        return new SupplierResponse(supplierRepository.save(supplier));
     }
 
     public void deleteSupplier(Long id) {
 
+        Supplier supplier = supplierRepository.findById(id)
+                .filter(Supplier::getState)
+                .orElseThrow(() -> new EntityNotFoundException("Supplier not found"));
+        supplier.setState(false);
+
+        supplierRepository.save(supplier);
     }
 }
