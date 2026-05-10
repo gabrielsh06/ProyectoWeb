7d767e1f926914c1837d6836b6aaa19fd448f63d
umaten <ccjesuslavado@gmail.com>
2026-05-10 16:47:15 -0500

    cuando las credenciales sean incorrectas devuelve un 401 en vez de 500

--- src/main/java/com/proyecto/web/auth/service/AuthService.java
+++ src/main/java/com/proyecto/web/auth/service/AuthService.java
@@ -4,7 +4,10 @@ import com.proyecto.web.auth.dto.request.LoginRequest;
 import com.proyecto.web.auth.dto.response.LoginResponse;
 import com.proyecto.web.auth.repository.AuthRepository;
 import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AuthService {
@@ -18,6 +21,7 @@ public class AuthService {
     public LoginResponse login(LoginRequest loginRequest) {
         return authRepository.findByUsernameAndPassword(loginRequest.username(), loginRequest.password())
                 .map(LoginResponse::new)
-                .orElseThrow(() -> new EntityNotFoundException("Username or password incorrect"));
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuario o contraseña incorrectos"));
     }
 }
